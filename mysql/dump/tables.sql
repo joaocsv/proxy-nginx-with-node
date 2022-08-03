28ebf37333b3939fbe712fa2c87f145f6aff1daf
Joao Carlos <joaoc.avils@gmail.com>
2022-08-03 15:46:23 -0300

    Adicionando nomes a cada refresh na pagina

--- mysql/dump/tables.sql
+++ mysql/dump/tables.sql
@@ -1,4 +1,4 @@
-CREATE table people (
+CREATE table peoples (
   id int not null auto_increment,
   name varchar(255) not null,
   primary key (id)
--- node/index.js
+++ node/index.js
@@ -1,10 +1,52 @@
 const express = require('express')
+const mysql = require('mysql')
+const random = require('random-name')
 
 const app = express()
 const port = 3000
 
+var mysqlConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+}
+
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>')
+  var connection = mysql.createConnection(mysqlConfig)
+
+  connection.connect()
+
+  connection.query(`INSERT INTO peoples (name) VALUE ('${ random.first() }')`)
+
+  const table = getPeoples(connection, (content) => {
+    res.send('<h1>Full Cycle Rocks!</h1>' + content)
+  })
+
+  connection.end()
 })
+
+function getPeoples (connection, callback) {
+  const query = connection.query('SELECT name FROM peoples', function (error, results, fields) {
+    if(error) return callback('<h1>could not capture the names<h2>');
+
+    if(results.length == 0) callback('<h1>Name list is empty<h2>');  
+
+    callback(mountTable(results))
+  })
+}
+
+function mountTable (results) {
+  table = '<table border="1">'
+  table += '<tr><th>names</th></tr>'
+  
+  for(var result of results) {
+    table += `<tr><td>${result.name}</td></tr>`  
+  }
+  
+  table += '</table>'
+
+  return table
+}
 
 app.listen(port, () => console.log(`Server running at ${port}`))
